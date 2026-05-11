b3c3475a7005e4bd03bfac6d621575130888a158
sidrasamatha2010-art <sidrasamatha2010@gmail.com>
2026-05-11 20:13:21 +0530

    Initial

---
+++ routine.py
@@ -0,0 +1,22 @@
+print("My Daily Routine")
+
+#Step 1:Waking up
+print("I wake up at 8:00 AM.")
+
+#Step 2:Eating breakfast
+print("I eat breakfast")
+
+#Step 3:Watching TV
+print("I watch TV")
+
+#Step 4:Playing jigsaw puzzle
+print("I play jigsaw puzzle")
+
+#Step 5:Playing football with my friends
+print("I play football with my friends")
+
+#Step 6:Sleeping
+print("I sleep at 10:00 PM")
+
+#Calculating toal study time in a week
+print("Total study hours in a week:", 2*7)
